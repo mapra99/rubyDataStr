d7f553fe9178c911eae53812d367c0ce20965ff3
Miguel Prada <mapra99@gmail.com>
2019-09-17 15:36:10 -0500

    Created Queue Data Structure

---
+++ lib/datastr/Queue.rb
@@ -0,0 +1,23 @@
+require 'LinkedList'
+
+class Queue
+  def initialize
+    @li = LinkedList.new
+    @n = 0
+  end
+
+  def add(number)
+    @li.add(number)
+    @n += 1
+  end
+
+  def remove
+    if(@n == 0)
+      return -1
+    else
+      value = @li.get(0)
+      @li.remove(0)
+      return value
+    end
+  end
+end
